80b4b8ff0fb9d3eadd4dee8f148b8d28656faa21
itsMohammedNayeem <nayeem.gmit@gmail.com>
2024-02-19 22:39:05 +0530

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import FormWithoutYup from "./components/FormWithoutYup"
+import FormWithoutYup from "./components/FormWithoutYup";
 
 function App() {
-
   return (
     <div className="font-mono items-center text-center justify-center">
-      <h1>Hello vite!</h1>
       <FormWithoutYup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
